1f13712600e12e70e189c067ed0c3cfc6d50369c
balam <bala.m15432@gmail.com>
2023-04-03 16:50:19 +0530

    Bus.java file is added

---
+++ src/test/java/com/foodies/buses/Bus.java
@@ -0,0 +1,19 @@
+package com.foodies.buses;
+
+public class Bus {
+
+	private int busNo;
+	public int getBusNo() {
+		return busNo;
+	}
+	public void setBusNo(int busNo) {
+		this.busNo = busNo;
+	}
+	public String getBusName() {
+		return busName;
+	}
+	public void setBusName(String busName) {
+		this.busName = busName;
+	}
+	private String busName;
+}
